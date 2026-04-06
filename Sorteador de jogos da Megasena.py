f992be84734fa85b28bec5c35dfefeeedf33d8ea
Diego Alencar <diegoalpereiraslz@gmail.com>
2026-04-05 21:11:33 -0300

    Sorteador de jogos da Megasena
    esse programa sorteia jogos da megasena de acordo com oque o usuário pedir.
    Use se sentir com sorte :)

---
+++ Sorteador de jogos da Megasena.py
@@ -0,0 +1,19 @@
+print('='*30)
+print('JOGO DA MEGA SENA')
+print('='*30)
+lista=[]
+dados=[]
+x = int(input('Quantos jogos você quer sortear? '))
+import random
+import time
+print(f'Sorteio dos {x} jogos')
+for c in range(0,x):
+    dados = random.sample(range(1,61), 6)
+    if dados in lista:
+        pass
+    else:
+        lista.append(dados[:])
+    dados.clear()
+    lista[c].sort()
+    print(f'Jogo {c+1}: {lista[c]}')
+    time.sleep(2)
